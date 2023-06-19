25c36337f9aabe8c7aa8d6625189038c1a7287f1
Ken <miyake.m.ken@gmail.com>
2023-06-19 18:06:27 -0400

    funcion contar palabras done

--- BestReviewsWordsCount.java
+++ BestReviewsWordsCount.java
@@ -1,6 +1,7 @@
 package org.mdp.spark.cli;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
@@ -46,8 +47,19 @@ public class BestReviewsWordsCount {
 		/*
 		 * Filtramos las mejores reseñas con titulo no nulo
 		 */
+		
+		//Guardamos en una variable la regex (por orden xDD)
+		
+		String reSplitData= ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+		
+		//Arreglo de stopwords (igual deberia de existir una forma mas elegante xDD)
+		
+		String[] stopwords = {"a", "as", "able", "about", "above", "according", "accordingly", "across", "actually", "after", "afterwards", "again", "against", "aint", "all", "allow", "allows", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "an", "and", "another", "any", "anybody", "anyhow", "anyone", "anything", "anyway", "anyways", "anywhere", "apart", "appear", "appreciate", "appropriate", "are", "arent", "around", "as", "aside", "ask", "asking", "associated", "at", "available", "away", "awfully", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "believe", "below", "beside", "besides", "best", "better", "between", "beyond", "both", "brief", "but", "by", "cmon", "cs", "came", "can", "cant", "cannot", "cant", "cause", "causes", "certain", "certainly", "changes", "clearly", "co", "com", "come", "comes", "concerning", "consequently", "consider", "considering", "contain", "containing", "contains", "corresponding", "could", "couldnt", "course", "currently", "definitely", "described", "despite", "did", "didnt", "different", "do", "does", "doesnt", "doing", "dont", "done", "down", "downwards", "during", "each", "edu", "eg", "eight", "either", "else", "elsewhere", "enough", "entirely", "especially", "et", "etc", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "ex", "exactly", "example", "except", "far", "few", "ff", "fifth", "first", "five", "followed", "following", "follows", "for", "former", "formerly", "forth", "four", "from", "further", "furthermore", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got", "gotten", "greetings", "had", "hadnt", "happens", "hardly", "has", "hasnt", "have", "havent", "having", "he", "hes", "hello", "help", "hence", "her", "here", "heres", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit", "however", "i", "id", "ill", "im", "ive", "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "indeed", "indicate", "indicated", "indicates", "inner", "insofar", "instead", "into", "inward", "is", "isnt", "it", "itd", "itll", "its", "its", "itself", "just", "keep", "keeps", "kept", "know", "knows", "known", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked", "likely", "little", "look", "looking", "looks", "ltd", "mainly", "many", "may", "maybe", "me", "mean", "meanwhile", "merely", "might", "more", "moreover", "most", "mostly", "much", "must", "my", "myself", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needs", "neither", "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone", "nor", "normally", "not", "nothing", "novel", "now", "nowhere", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "ones", "only", "onto", "or", "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out", "outside", "over", "overall", "own", "particular", "particularly", "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provides", "que", "quite", "qv", "rather", "rd", "re", "really", "reasonably", "regarding", "regardless", "regards", "relatively", "respectively", "right", "said", "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "she", "should", "shouldnt", "since", "six", "so", "some", "somebody", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", "ts", "take", "taken", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "thats", "thats", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "theres", "thereafter", "thereby", "therefore", "therein", "theres", "thereupon", "these", "they", "theyd", "theyll", "theyre", "theyve", "think", "third", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "twice", "two", "un", "under", "unfortunately", "unless", "unlikely", "until", "unto", "up", "upon", "us", "use", "used", "useful", "uses", "using", "usually", "value", "various", "very", "via", "viz", "vs", "want", "wants", "was", "wasnt", "way", "we", "wed", "well", "were", "weve", "welcome", "well", "went", "were", "werent", "what", "whats", "whatever", "when", "whence", "whenever", "where", "wheres", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whos", "whoever", "whole", "whom", "whose", "why", "will", "willing", "wish", "with", "within", "without", "wont", "wonder", "would", "would", "wouldnt", "yes", "yet", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves", "zero"};
+		
+		List<String> stopwordsList = Arrays.asList(stopwords);
+		
 		JavaRDD<String> bestReviews = inputRDD.filter(
-				line -> !line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)[1].equals("") && Double.parseDouble(line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)[6])>3
+				line -> !line.split(reSplitData, -1)[1].equals("") && Double.parseDouble(line.split(reSplitData, -1)[6])>3
 		);
 		
 		/*
@@ -57,8 +69,8 @@ public class BestReviewsWordsCount {
 
 		JavaRDD<Tuple2<String,String>> reviewsScoreText = bestReviews.map(
 				line -> new Tuple2<String,String> (
-							line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)[1],
-							line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)[9].toLowerCase()
+							line.split(reSplitData, -1)[1],
+							line.split(reSplitData, -1)[9].toLowerCase()
 						)
 		);
 	
@@ -68,8 +80,25 @@ public class BestReviewsWordsCount {
 		JavaRDD<String> wordRDD = reviewsScoreText.flatMap(tup -> Arrays.<String>asList(tup._2.split("\\W+")).iterator());
 		
 		JavaPairRDD<String,Integer> wordOneRDD = wordRDD.mapToPair(word -> new Tuple2<String,Integer>(word, 1));
-		JavaPairRDD<String,Integer> wordCount = wordOneRDD.reduceByKey((a, b) -> a + b);
-		wordCount.saveAsTextFile(outputFilePath);
+		
+		
+		//filtramos las stopwords(hasta ahora funcionaba sin el segundo condicional, falta probar con los ultimos dos)
+		
+		JavaPairRDD<String,Integer> wordOneFilteredRDD = wordOneRDD.filter(tup -> !stopwordsList.contains(tup._1) && tup._1.length() !=0 && tup._1.length() !=1);
+		
+		
+		
+		
+		JavaPairRDD<String,Integer> wordCount = wordOneFilteredRDD.reduceByKey((a, b) -> a + b);
+		
+		//Aqui vamos a ordenar por cantidad de palabras
+		
+		JavaPairRDD<Integer, String> keyInvertedWordCount= wordCount.mapToPair(tup -> new Tuple2<Integer,String> (tup._2,tup._1));
+		JavaPairRDD<Integer,String> keyInvertedWordCountOrdered= keyInvertedWordCount.sortByKey(false);
+		
+		//guardamos nuestro ultimo resultado
+		
+		keyInvertedWordCountOrdered.saveAsTextFile(outputFilePath);
 		
 		context.close();		
 		
